33dcd6ce9491782718311fdf3bf113cab8f6f3a4
jordanbertasso <36979824+jordanbertasso@users.noreply.github.com>
2021-09-12 22:15:44 +1000

    Update config

--- ctfd-first-blood-discord/config.py
+++ ctfd-first-blood-discord/config.py
@@ -11,13 +11,12 @@ poll_period = 10
 webhook_url = ""
 
 # Available template variables are:
-# User Name: {user_name}
+# User Name (if individual mode) / Team Name (if team mode): {user_name}
 # Challenge Name: {chal_name}
-# Team Name: {team_name}
-first_blood_announce_string = "First Blood for challenge **{chal_name}** goes to **{user_name}** from **{team_name}**! :knife: :drop_of_blood:"
+first_blood_announce_string = "First Blood for challenge **{chal_name}** goes to **{user_name}**! :knife: :drop_of_blood:"
 
 # To be used with announce_all_solves
-solve_announce_string = "**{user_name}** from **{team_name}** just solved **{chal_name}**!"
+solve_announce_string = "**{user_name}** just solved **{chal_name}**!"
 
 # Whether or not to announce all solves as well as first bloods
 announce_all_solves = True
