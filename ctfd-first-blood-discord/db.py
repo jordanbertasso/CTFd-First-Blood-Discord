70fa649c6ebbf1380ea9a6987492e27b6477c000
jordanbertasso <36979824+jordanbertasso@users.noreply.github.com>
2022-09-11 21:14:13 +1000

    Change db file extension

--- ctfd-first-blood-discord/db.py
+++ ctfd-first-blood-discord/db.py
@@ -6,7 +6,7 @@ from user import User
 
 
 class db:
-    conn = sqlite3.connect('./data/ctfd-solve-announcer.db')
+    conn = sqlite3.connect('./data/ctfd-solve-announcer.sqlite')
     cursor = conn.cursor()
 
     @staticmethod
